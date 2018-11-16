9805182784fc53a3ef13b022c774e4d207dba054
David <dhoc1901@mpsedu.org>
2018-11-16 13:09:46 -0600

    Minor Changes and Work

--- app/src/main/java/com/dw/wmun1901/synthesizer/SynthesizerActivity.java
+++ app/src/main/java/com/dw/wmun1901/synthesizer/SynthesizerActivity.java
@@ -2,12 +2,22 @@ package com.dw.wmun1901.synthesizer;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Button;
 
 public class SynthesizerActivity extends AppCompatActivity {
+    private static final String TAG =
+SynthesizerActivity.class.getName();
+    private Button button1;
+    private Button button2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_synthesizer);
+
+        button1 = (Button)findViewById(R.id.button1);
+        button2 = (Button)findViewById(R.id.button2);
     }
 }
