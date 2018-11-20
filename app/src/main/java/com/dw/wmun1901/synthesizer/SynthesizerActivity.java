419108bcb87f6dbf89f229352921d381830dd50d
David <dhoc1901@mpsedu.org>
2018-11-20 13:08:03 -0600

    Up to part V

--- app/src/main/java/com/dw/wmun1901/synthesizer/SynthesizerActivity.java
+++ app/src/main/java/com/dw/wmun1901/synthesizer/SynthesizerActivity.java
@@ -2,8 +2,10 @@ package com.dw.wmun1901.synthesizer;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 import android.widget.Button;
 
 public class SynthesizerActivity extends AppCompatActivity {
@@ -17,7 +19,19 @@ SynthesizerActivity.class.getName();
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_synthesizer);
 
-        button1 = (Button)findViewById(R.id.button1);
-        button2 = (Button)findViewById(R.id.button2);
+        button1 = findViewById(R.id.button1);
+        button2 = findViewById(R.id.button2);
+        button1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Log.i(TAG, "Button 1 Clicked");
+            }
+        });
+        button2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Log.i(TAG, "Button 2 Clicked");
+            }
+        });
     }
 }
