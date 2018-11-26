cbf25e17f6962d059bbe1802ca3986a75afd6ea9
williammunnich <williammunnich@gmail.com>
2018-11-26 13:09:18 -0600

    kinda added click funtionality

--- app/src/main/java/com/dw/wmun1901/synthesizer/SynthesizerActivity.java
+++ app/src/main/java/com/dw/wmun1901/synthesizer/SynthesizerActivity.java
@@ -7,24 +7,33 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
 public class SynthesizerActivity extends AppCompatActivity {
     private static final String TAG =
 SynthesizerActivity.class.getName();
     private Button button1;
     private Button button2;
+    private TextView BackgroundText; //BackgroundText
+    private int Ammount = 1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_synthesizer);
 
-        button1 = findViewById(R.id.button1);
-        button2 = findViewById(R.id.button2);
+        button1 = (Button)findViewById(R.id.button1);
+        button2 = (Button)findViewById(R.id.button2);
+        BackgroundText = (TextView)findViewById(R.id.BackgroundText);
+
+
         button1.setOnClickListener(new View.OnClickListener() {
             @Override
+
             public void onClick(View v) {
                 Log.i(TAG, "Button 1 Clicked");
+                BackgroundText.setText("Button 1 Clicked" + Ammount);
+                Ammount = Ammount ++;
             }
         });
         button2.setOnClickListener(new View.OnClickListener() {
